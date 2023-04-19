3fe3ff7650d2c6d53fecb0d26a0a0f00db301c62
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-04-18 17:00:41 -0700

    Adds create customer subscription endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+      resources :customer_subscriptions, only: [:create]
+  # post "/customer_subscriptions/:customer_id/:subscription_id", to: "customer_subscriptions#create"
 end

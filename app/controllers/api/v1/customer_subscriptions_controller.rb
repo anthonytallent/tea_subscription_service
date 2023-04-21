a1319f270b1899cbc3a789063e95503065487843
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-04-21 07:51:35 -0700

    fix models and controllers logic

--- app/controllers/api/v1/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customer_subscriptions_controller.rb
@@ -9,4 +9,11 @@ class Api::V1::CustomerSubscriptionsController < ApplicationController
     end
   end
 
+  def update
+    cust_sub = CustomerSubscription.find_by(customer_id: params["customer_id"], subscription_id: params["subscription_id"])
+    # subscription = cust_sub.subscription
+    cust_sub.update(status: 1)
+    render json: CustomerSubscriptionSerializer.new(cust_sub)
+  end
+
 end
--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::SubscriptionsController < ApplicationController
   def update
     cust_sub = CustomerSubscription.find_by(customer_id: params["customer_id"], subscription_id: params["subscription_id"])
-    subscription = cust_sub.subscription
-    subscription.update(status: 1)
-    render json: SubscriptionSerializer.new(subscription)
+    # subscription = cust_sub.subscription
+    cust_sub.update(status: 1)
+    render json: CustomerSubscriptionSerializer.new(cust_sub)
   end
 end
